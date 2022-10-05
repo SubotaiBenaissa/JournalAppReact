dbcc70f8127f2cb88f1b0d6263c99ff025fcf4be
Alangobe <alangobe01@gmail.com>
2022-10-05 15:10:20 -0500

    Modificado RegisterPage

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,5 +1,4 @@
 import { Link as RouterLink } from 'react-router-dom'
-import { Google } from '@mui/icons-material'
 import { Button, Grid, Link, TextField, Typography } from '@mui/material'
 import React from 'react'
 import { AuthLayout } from '../layout/AuthLayout'
@@ -40,21 +39,16 @@ export const RegisterPage = () => {
                         spacing={ 2 }
                         sx={{ mt: 1, mb: 1 }}
                     >
-                        <Grid item xs={ 12 } sm={ 6 }>
+                        <Grid item xs={ 12 }>
                             <Button variant="contained" fullWidth>
-                                <Typography>Login</Typography>
-                            </Button>
-                        </Grid>
-                        <Grid item xs={ 12 } sm={ 6 }>
-                            <Button variant="contained" fullWidth>
-                                <Google />
-                                <Typography sx={{ml: 1}}>Google</Typography>
+                                <Typography>Register</Typography>
                             </Button>
                         </Grid>
                     </Grid>
                     <Grid container direction="row" justifyContent="end">
-                        <Link component={ RouterLink } color="inherit" to="/auth/register">
-                            Register
+                        <Typography sx={{ mr: 1 }}>Already have an account?</Typography>
+                        <Link component={ RouterLink } color="inherit" to="/auth/login">
+                            Log in
                         </Link>
                     </Grid>
                 </Grid>
